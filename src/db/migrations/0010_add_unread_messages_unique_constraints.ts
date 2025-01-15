109e608f66ff8cc846ccfdd4122b6deb0cff28e8
Cale McNulty <cale.mcnulty@gauntletai.com>
2025-01-14 22:51:33 -0800

    sanity

--- src/db/migrations/0010_add_unread_messages_unique_constraints.ts
+++ src/db/migrations/0010_add_unread_messages_unique_constraints.ts
@@ -3,12 +3,12 @@ import { db } from '../index'
 
 export async function up() {
   await db.execute(sql`
-    CREATE UNIQUE INDEX unread_messages_user_id_channel_id_key 
-    ON unread_messages (user_id, channel_id) 
-    WHERE channel_id IS NOT NULL;
-    
-    CREATE UNIQUE INDEX unread_messages_user_id_dm_channel_id_key 
-    ON unread_messages (user_id, dm_channel_id) 
-    WHERE dm_channel_id IS NOT NULL;
+    ALTER TABLE unread_messages 
+    ADD CONSTRAINT unread_messages_user_id_channel_id_key 
+    UNIQUE (user_id, channel_id);
+
+    ALTER TABLE unread_messages 
+    ADD CONSTRAINT unread_messages_user_id_dm_channel_id_key 
+    UNIQUE (user_id, dm_channel_id);
   `)
 } 
