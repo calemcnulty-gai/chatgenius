ff37019e72b778fec799154f4c1d624c129d7376
Cale McNulty <cale.mcnulty@gauntletai.com>
2025-01-14 23:26:36 -0800

    migration and styling

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -19,6 +19,7 @@ const migrations = [
   { up: addNateDiaz, name: 'Nate Diaz migration' },
   { up: addUnreadMessagesConstraints, name: 'add unread messages constraints' },
   { up: updateAiProfileImages, name: 'update AI profile images to relative paths' },
+]
 
 async function main() {
   try {
